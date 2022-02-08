8f22d3a22fffcad4fccd3e1cbf8e598726f0f2bf
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-02-08 19:56:08 +0530

    boostrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
         doc tracker
+        <Button variant="primary">Primary</Button>
     </div>
   );
 }
