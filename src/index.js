9a710a37b04f8c912f3de8bc5bb0d5927409b006
sangeethnavodya <pererakasn.19@uom.lk>
2022-05-10 15:49:32 +0530

    hay

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+//setup redux store
+const store=configureStore({
+  reducer:{}
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
