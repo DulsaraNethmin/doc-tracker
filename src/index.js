edee2dac2bff879fe2e9ef8a668d5b6c7427ed63
sangeethnavodya <pererakasn.19@uom.lk>
2022-05-10 16:04:01 +0530

    addprovider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {configureStore} from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 //setup redux store
 const store=configureStore({
   reducer:{}
@@ -9,7 +10,9 @@ const store=configureStore({
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
