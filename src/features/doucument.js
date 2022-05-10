c0a5f17b8e8244b1880303125c4c9b7f7ffd5db2
sangeethnavodya <pererakasn.19@uom.lk>
2022-05-10 18:19:43 +0530

    created documentSlice

--- src/features/doucument.js
+++ src/features/doucument.js
@@ -1,1 +1,18 @@
-hay
+//to create slice
+import {createSlice} from '@reduxjs/toolkit';
+
+export const documentSlice=createSlice({
+    name:"document",
+    initialState: {value:{
+        uuid:0,
+        doc_name : "",
+        type:"",
+        date:"2022-01-01", 
+    }},
+    reducers : {
+        document : (state,action)=>{
+              state.value=action.payload;
+        }
+
+    }
+})
