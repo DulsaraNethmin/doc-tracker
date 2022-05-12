3ff936aff69f7e84f8f2d43a1fc99cc41ea95363
sangeethnavodya <pererakasn.19@uom.lk>
2022-05-12 17:16:34 +0530

    fake

---
+++ FakeData/documentFakedata.js
@@ -0,0 +1,45 @@
+export const documentData=[
+    {
+        id:1,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:2,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:3,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:4,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:5,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:6,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    },
+    {
+        id:7,
+        doc_name:"Sinhala Aurudu Report",
+        type:"Text",
+        Added_date:"2022-01-22"
+    }
+
+];
