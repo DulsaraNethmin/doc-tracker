df63a77509f1afef7df9803785dcac3bdbedfb99
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-02 14:33:21 +0530

    installed antd and modified App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Chat from './components/Chat';
-import { BrowserRouter as Router,Routes, Route, BrowserRouter } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Document from './components/Document';
-import Notification from './components/Notification';
+import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Chat from "./components/Chat";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Document from "./components/Document";
+import Notification from "./components/Notification";
+import AdminLoginPage from "./screens/AdminLoginPage";
+import AdminRegPage from "./screens/AdminRegPage";
+import OrgCreatePage from "./screens/OrgCreatePage";
+import SplashScreenPage from "./screens/SplashScreenPage";
+import UserRegPage from "./screens/UserRegPage";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
-       <Routes>
-        <Route path="/documents" exact element={<Document/>} />
-        <Route path="/chat" exact element={<Chat />} />
-        <Route path="/notification" exact element={<Notification />} />
-
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<SplashScreenPage />} />
+          <Route exact path="/login" element={<AdminLoginPage />} />
+          <Route exact path="/create-organization" element={<OrgCreatePage />} />
+          <Route exact path="/register-admin" element={<AdminRegPage />} />
+          <Route path="/documents" exact element={<Document />} />
+          <Route path="/chat" exact element={<Chat />} />
+          <Route path="/notification" exact element={<Notification />} />
         </Routes>
-      <Chat/>
-      <Footer/>
+        <Chat />
+        <Footer />
       </BrowserRouter>
     </div>
   );
