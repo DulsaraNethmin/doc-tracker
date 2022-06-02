3b1c53396f3fd3532cbf9720c93c39d140e37fe1
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-02 13:59:18 +0530

    Created OrgCreate Page

---
+++ src/screens/OrgCreatePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Row, Col } from "antd";
+import { Card } from "antd";
+import OrgCreate from '../components/OrgCreate';
+
+const logo = new URL("../images/logo.png", import.meta.url);
+
+const OrgCreatePage = () => {
+  return (
+    <div className="main-container">
+      <h1>DOC-TRACKER</h1>
+      <img src={logo} className="image-container" />
+      <OrgCreate />
+    </div>
+  )
+}
+
+export default OrgCreatePage
