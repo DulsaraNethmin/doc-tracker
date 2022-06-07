77820ab49c97f80699ab031a82e9115ba9e0d211
rankothnc <ncrankoth@gmail.com>
2022-06-07 10:12:30 +0530

    Added 3 cards

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Dashboard/Header";
+import Feactures from "../components/Dashboard/Features";
 
 function Dashboard(){
   return(
     <div className="main">
       <Header/>
+      <Feactures/>
     </div>
   );
 }
