df84429a7cffa3c7b99a620cde015839bd5b3cba
rankothnc <ncrankoth@gmail.com>
2022-06-08 21:17:50 +0530

    Deleted features.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Dashboard/Header";
-import Feactures from "../components/Dashboard/Features";
+// import Feactures from "../components/Dashboard/Features";
 
 function Dashboard(){
   return(
     <div className="main">
       <Header/>
-      <Feactures/>
+      {/* <Feactures/> */}
     </div>
   );
 }
