ef0cfbb55e0b0b027e48c4ce06af517c3745c212
rankothnc <ncrankoth@gmail.com>
2022-06-09 17:50:25 +0530

    widget2

--- src/components/Dashboard/Widget.js
+++ src/components/Dashboard/Widget.js
@@ -117,10 +117,7 @@ const data = [
     },
     {
         key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
+        
     },
 ];
 
