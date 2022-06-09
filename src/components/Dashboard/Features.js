d3d5e62724e69cda1cb6c07c5a00d3f60c8af1e3
rankothnc <ncrankoth@gmail.com>
2022-06-09 07:07:15 +0530

    Added card icons

--- src/components/Dashboard/Features.js
+++ src/components/Dashboard/Features.js
@@ -2,71 +2,40 @@ import React from 'react';
 import "antd/dist/antd.css";
 import { Row, Col } from 'antd';
 import { Card } from 'antd';
+import { TeamOutlined, UnorderedListOutlined, SyncOutlined } from '@ant-design/icons';
 const { Meta } = Card;
+
 
 function Features() {
   return (
     <div id="feature" className="block featureBlock bgGray">
       <div className="container-fluid">
-        {/* <Row>
+        <Row>
           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
             <Card
-              hoverable
-        
-            >
+              hoverable>
               <Meta title="Jobs to be completed" />
+              <UnorderedListOutlined /> <p>10</p>
             </Card>
           </Col>
           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
             <Card
-              hoverable
-              
-            >
+              hoverable>
               <Meta title="Employees available" />
+              <TeamOutlined /><p>05</p>
             </Card>
           </Col>
           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
             <Card
-              hoverable
-              
-            >
+              hoverable>
               <Meta title="Job requests" />
+              <SyncOutlined spin /><p>03</p>
             </Card>
           </Col>
-          
-        </Row> */}
-        <Row gutter={[32, 16]}>
-  <Col span={6}>
-  <Card
-    hoverable
-    style={{ width: 240 }}>
-      
-    <Meta title="Jobs to be completed" />
-    <br />
-    <p>10</p>
-  </Card>
-  </Col>
-  
-  <Col span={6}>
-  <Card
-    hoverable
-    style={{ width: 240 }}>
-    <Meta title="Employees available" />
-    <br />
-    <p>10</p>
-  </Card>
-  </Col>
-  
-  <Col span={6}>
-  <Card
-    hoverable
-    style={{ width: 240 }}>
-    <Meta title="Job requests" /><br />
-    <p>10</p>
-  </Card>
-  </Col>
 
-</Row>
+
+
+        </Row>
       </div>
     </div>
   );
@@ -74,50 +43,3 @@ function Features() {
 
 export default Features;
 
-
-// import "antd/dist/antd.css";
-// import React from 'react';
-// import { Row, Col } from 'antd';
-// import { Card } from 'antd';
-// import { Button } from 'antd';
-// const { Meta } = Card;
-
-// function AppFeature() {
-//   return (
-//     <div id="feature" className="block featureBlock bgGray">
-//       <Button type="primary">Primary Button</Button>
-//       <div className="container-fluid">
-
-//         <Row gutter={[16,16]}>
-//           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-//             <Card
-//               hoverable
-           
-//             >
-//               <Meta title="Modern Design" />
-//             </Card>
-//           </Col>
-//           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-//             <Card
-//               hoverable
-             
-//             >
-//               <Meta title="Clean and Elegant" />
-//             </Card>
-//           </Col>
-//           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
-//             <Card
-//               hoverable
-             
-//             >
-//               <Meta title="Great Support" />
-//             </Card>
-//           </Col>
-          
-//         </Row>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AppFeature;
