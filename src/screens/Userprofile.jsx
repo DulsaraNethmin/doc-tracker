3ea3a7b4f8a6e11b71337ade8b441415561fbedb
rankothnc <ncrankoth@gmail.com>
2022-06-15 00:46:19 +0530

    Changed user profile

--- src/screens/Userprofile.jsx
+++ src/screens/Userprofile.jsx
@@ -10,6 +10,11 @@ import {
   InputNumber,
   TreeSelect,
   Switch,
+  Steps,
+  Step,
+  Row,
+  Col,
+  Card,
   Checkbox,
 } from 'antd';
 import "../components/User.css"
@@ -25,48 +30,53 @@ const UserProfile = ({props}) => {
   };
 
   return (
-    <div className='UserProf'>
-        <Form
-      labelCol={{
-        span: 4,
-      }}
-      wrapperCol={{
-        span: 14,
-      }}
-      layout="horizontal"
-      initialValues={{
-        disabled: componentDisabled,
-      }}
-      onValuesChange={onFormLayoutChange}
-      disabled={componentDisabled}
-    >
-      <Form.Item label="ID">
-        <Input value={props.id}/>
-      </Form.Item>
-      <Form.Item label="Name">
-        <Input value={props.name}/>
-      </Form.Item>
-      <Form.Item label="User name">
-        <Input value={props.username}/>
-      </Form.Item>
-      <Form.Item label="Email">
-        <Input value={props.email}/>
-      </Form.Item>
-      <Form.Item label="Password">
-        <Input value={props.password}/>
-      </Form.Item>
-      <Form.Item label="Role">
-        <Input value={props.role}/>
-      </Form.Item>
-      <Form.Item label="Telephone">
-        <Input value={props.telephone}/>
-      </Form.Item>     
-    </Form>
-<div className='saveButton'>
+    <div className='Background'>
+    <div className='Cards'>
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card title="Domain" bordered={false}>
+                    <Form>
+                    <Form.Item label="Id" name="customer_id">
+                            <label htmlFor=""></label>
+                        </Form.Item>
+
+                        <Form.Item label="Name" name="id">
+                            <label htmlFor=""></label>
+                        </Form.Item>
+
+                        <Form.Item label="User Name" name="admin_id">
+                            <label htmlFor=""></label>
+                        </Form.Item>
+
+                        <Form.Item label="Email" name="customer_id">
+                            <label htmlFor=""></label>
+                        </Form.Item>
+
+                        <Form.Item label="Telephone" name="customer_id">
+                            <label htmlFor=""></label>
+                        </Form.Item>
+
+                    </Form>
+                </Card>
+            </Col>
+
+            <Col span={8}>
+                <Card title="Datails" bordered={false}>
+                    {/* <Steps direction="vertical" current={1}>
+                        <Step title="Finished" description="Order recieved" />
+                        <Step title="In Progress" description="Order confirmed" />
+                        <Step title="Waiting" description="Desitination 1" />
+                        <Step title="Waiting" description="Destination 2" />
+                        <Step title="Waiting" description="Received" />
+                    </Steps> */}
+                </Card>
+            </Col>
+        </Row>
+        <div className='saveButton'>
 <Button type="primary">Save Changes</Button>
 </div>
-    
     </div>
+</div>
   );
 };
 
