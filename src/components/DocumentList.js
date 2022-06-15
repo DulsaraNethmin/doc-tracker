38dc5f848195ca2468a744e9914c4ba4aac91491
sangeethnavodya <pererakasn.19@uom.lk>
2022-06-15 22:06:33 +0530

    create table

--- src/components/DocumentList.js
+++ src/components/DocumentList.js
@@ -1,63 +1,66 @@
 import React from 'react'
-import {useSelector,useDispatch} from 'react-redux'
-import {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import { fetchDocuments } from '../features/doucument';
 import { Link } from 'react-router-dom';
-
+import './DocumentListcs.css'
 
 function DocumentList() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const getdocument=useSelector(state=>state.documents);
-    const adddocument=useSelector(state=>state.documents);
-    useEffect(() => {
-        dispatch(fetchDocuments())
-    }, [dispatch]);
-  
+
+  const getdocument = useSelector(state => state.documents);
+  const adddocument = useSelector(state => state.documents);
+  useEffect(() => {
+    dispatch(fetchDocuments())
+  }, [dispatch]);
+
   return (
     <div>
-         <div>
-           
-         <Link to='/documents'>
-              <button>adddocument</button>
-              </Link>
-          {getdocument.status==="pending"&&<div>ERROR</div>}
-          {(getdocument.status==="Success" || adddocument.documentAdd==="Succses")&&
+      <div>
+
+        <Link to='/documents'>
+          <button>adddocument</button>
+        </Link>
+        {getdocument.status === "pending" && <div>ERROR</div>}
+        {(getdocument.status === "Success" || adddocument.documentAdd === "Succses") &&
           <div>
-              {console.log(getdocument.DocumentList)}
-            {getdocument.Documents.map(doc=>
-              <div>
-                <table>
-  <tr>
-    <th>Document_name</th>
-    <th>Type</th>
-    <th>Date</th>
-    <th>Department</th>
-    <th>Iscompleted</th>
-  </tr>
-  <td>
-    <tr>{doc.doc_name}</tr>
-  </td>
-  <td>
-    <tr>{doc.type}</tr>
-  </td>
-  <td>
-    <tr>{doc.date}</tr>
-  </td>
-  <td>
-    <tr>{doc.department_id}</tr>
-  </td>
-  <td>
-    <tr>
-      <button>Completed</button>
-    </tr>
-  </td>
-</table>
-              </div>)}     
-            </div>}
-          </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Document_Name</th>
+                  <th>Type</th>
+                  <th>Date</th>
+                  <th>Department_id</th>
+                </tr>
+              </thead>
+              <tbody>
+                {getdocument.Documents.map(doc => <div>
+                  <td>
+                    <tr>{doc.doc_name}</tr>
+                  </td>
+                  <td>
+                    <tr>{doc.type}</tr>
+                  </td>
+                  <td>
+                    <tr>{doc.date}</tr>
+                  </td>
+                  <td>
+                    <tr>{doc.department_id}</tr>
+                  </td>
+                  <td>
+                    <tr>
+                      <button>Completed</button>
+                    </tr>
+                  </td>
+
+                </div>)}
+              </tbody>
+            </table>
+          </div>}
+      </div>
     </div>
-    
+
   )
 }
 
