752c811f1b82c148747f26cea3a383754cb5e1f4
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-15 18:19:49 +0530

    Added logout button on dashboards

--- src/screens/OrganizationDashboard.jsx
+++ src/screens/OrganizationDashboard.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import { Button } from 'antd';
+import React from "react";
+import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
 
 const OrganizationDashboard = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <Button 
-      onClick={(e) => {
-        navigate("/Branch-reg");
-      }}
-      type="primary">Create Branch</Button>
+      <Button
+        onClick={(e) => {
+          navigate("/Branch-reg");
+        }}
+        type="primary"
+      >
+        Create Branch
+      </Button>
       <br />
       <Button type="primary">Branches</Button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button
+        type="primary"
+        htmlType="submit"
+        className="login-form-button"
+        onClick={async (e) => {
+          e.preventDefault();
+          localStorage.clear();
+          navigate("/");
+        }}
+      >
+        Organization Logout
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default OrganizationDashboard
+export default OrganizationDashboard;
