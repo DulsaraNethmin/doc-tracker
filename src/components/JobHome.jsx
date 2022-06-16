cd744a28d17b47fd39fa6c43e1791b0083cd85ff
rankothnc <ncrankoth@gmail.com>
2022-06-16 08:58:08 +0530

    changed 2 buttons

--- src/components/JobHome.jsx
+++ src/components/JobHome.jsx
@@ -8,8 +8,8 @@ function JobHome() {
   return (
     <div className='Background'>
       <div className='button'>
-      <Button className='btn1' type="primary" onClick={(e) => {navigate("JobsInProgress");}}>Jobs in progress</Button>
-      <Button className='btn2' type="primary" onClick={(e) => {navigate("NewJobRequests");}}>New job requests</Button>
+      <Button className='btn1' type="primary" onClick={(e) => {navigate("NewJobRequests");}}>New job requests</Button>
+      <Button className='btn2' type="primary" onClick={(e) => {navigate("JobsInProgress");}}>Jobs in progress</Button>
       <Button className='btn3' type="primary" onClick={(e) => {navigate("CompletedJobs");}}>Completed jobs</Button>
       {/* <Button className='btn4' type="primary">Job tracking</Button> */}
       </div>
