93e0a0a4576884db701ded2ff9cdb4bf7a9ec103
rankothnc <ncrankoth@gmail.com>
2022-06-20 14:09:28 +0530

    Jobs home added 2 cards

--- src/screens/Jobs/Jobs.jsx
+++ src/screens/Jobs/Jobs.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import JobHome from '../../components/JobHome';
+import { Col, Divider, Row, Card, Button, Checkbox, Form, Input, Steps } from 'antd';
+// import "./components/Job.css";
 
 function Jobs() {
     return (
         <div>
-            <JobHome />
-            {/* <Route exact path="/" element={<SplashScreenPage />} /> */}
+            {/* <JobHome /> */}
+            <div className='Background'>
+                <div className='Cards'>
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Card bordered={false}>
+                                <h1>Internal Jobs</h1>               
+                            </Card>
+                        </Col>
+
+                        <Col span={8}>
+                            <Card bordered={false}>
+                                <h1>External jobs</h1>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                </div>
+            </div>
+
         </div>
     )
 }
