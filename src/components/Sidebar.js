8cd888da0dc105184a37e440ed6963b2ab3068dd
rankothnc <ncrankoth@gmail.com>
2022-06-20 10:18:16 +0530

    Sidebar changed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -68,3 +68,78 @@
 // }
 
 // export default Sidebar;
+
+
+
+
+
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  FileTextOutlined,
+  PieChartOutlined,
+  CreditCardOutlined,
+  CarryOutOutlined,
+  UserOutlined,
+  BellOutlined,
+  AppstoreOutlined,
+  WechatOutlined
+} from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+ 
+  getItem(<a class="nav-link" href="/documents">Documents</a>,'0',<FileTextOutlined />),
+
+  getItem(<a class="nav-link" href="">User</a>, '2', <UserOutlined />),
+
+  getItem('Jobs', 'sub2', <AppstoreOutlined />, [
+    getItem(<a class="nav-link" href="">Internal Jobs</a>, 'sub3', null, [getItem('New Requests', '7'), getItem('In progress', '8'),getItem('Completed', '9')]),
+    getItem(<a class="nav-link" href="">External Jobs</a>, 'sub4', null, [getItem('New Requests', '10'), getItem('In progress', '11'),getItem('Completed', '12')]),
+  ]),
+
+  getItem( <a class="nav-link" href="/notification">Notifications</a>, '3',<BellOutlined />),
+  getItem( <a class="nav-link" href="/chat">Chat</a>, '4',<WechatOutlined />),
+];
+
+class Sidebar extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({
+      collapsed,
+    });
+  };
+
+  render() {
+    const { collapsed } = this.state;
+    return (
+        
+      <Layout
+        style={{
+          minHeight: '100vh',
+        }}
+      >
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu className="Sidebar" theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        </Sider>
+        <Layout className="site-layout">
+          
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default () => <Sidebar />;
