6867b8fa3b73027746ffffa818f91081d6ed3ec1
rankothnc <ncrankoth@gmail.com>
2022-06-20 19:20:55 +0530

    Profile form changed

--- src/screens/Userprofile.jsx
+++ src/screens/Userprofile.jsx
@@ -1,75 +1,88 @@
 import React, { useState } from 'react';
+import "./pages.css";
 import {
-  Form,
-  Input,
-  Button,
-  Radio,
-  Select,
-  Cascader,
-  DatePicker,
-  InputNumber,
-  TreeSelect,
-  Switch,
-  Steps,
-  Step,
-  Row,
-  Col,
-  Card,
-  Checkbox,
+    Form,
+    Input,
+    Button,
+    Radio,
+    Select,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    TreeSelect,
+    Switch,
+    Steps,
+    Step,
+    Row,
+    Col,
+    Card,
+    Checkbox,
+    Image
 } from 'antd';
 import "../components/User.css"
 
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
-const UserProfile = ({props}) => {
-  const [componentDisabled, setComponentDisabled] = useState(true);
+const UserProfile = ({ props }) => {
+    const [componentDisabled, setComponentDisabled] = useState(true);
 
-  const onFormLayoutChange = ({ disabled }) => {
-    setComponentDisabled(disabled);
-  };
+    const onFormLayoutChange = ({ disabled }) => {
+        setComponentDisabled(disabled);
+    };
 
-  return (
-    <div className='Background'>
-    <div className='Cards'>
-        <Row gutter={16}>
-            <Col span={8}>
-                <Card title="User_Name" bordered={false}>
-                    <Form>
-                    <Form.Item label="Id" name="customer_id">
-                            <label htmlFor=""></label>
-                        </Form.Item>
+    return (
+        <div className='Background'>
+            <div className='Cards'>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card title="User_Name" bordered={false}>
+                            <Form>
+                                <Form.Item name="customer_id">
+                                    <label htmlFor=""></label>
+                                    <Image
+                                        width={200}
+                                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                                    />
+                                </Form.Item>
+                                <Form.Item label="Id" name="customer_id">
+                                    <label htmlFor=""><Input /></label>
+                                </Form.Item>
 
-                        <Form.Item label="Name" name="id">
-                            <label htmlFor=""></label>
-                        </Form.Item>
+                                <Form.Item label="Name" name="id">
+                                    <label htmlFor=""><Input  /></label>
+                                </Form.Item>
 
-                        <Form.Item label="User Name" name="admin_id">
-                            <label htmlFor=""></label>
-                        </Form.Item>
+                                <Form.Item label="User Name" name="admin_id">
+                                    <label htmlFor=""><Input  /></label>
+                                </Form.Item>
 
-                        <Form.Item label="Email" name="customer_id">
-                            <label htmlFor=""></label>
-                        </Form.Item>
+                                <Form.Item label="Email" name="customer_id">
+                                    <label htmlFor=""><Input  /></label>
+                                </Form.Item>
 
-                        <Form.Item label="Telephone" name="customer_id">
-                            <label htmlFor=""></label>
-                        </Form.Item>
+                                <Form.Item label="Telephone" name="customer_id">
+                                    <label htmlFor=""><Input  /></label>
+                                </Form.Item>
+                                <Form.Item label="Job role" name="customer_id">
+                                    <label htmlFor=""><Input  /></label>
+                                </Form.Item>
 
-                    </Form>
-                </Card>
-            </Col>
+                            </Form>
+                            <Button>Save changes</Button>
+                        </Card>
+                    </Col>
 
-            <Col span={8}>
-               
-            </Col>
-        </Row>
-        <div className='saveButton'>
-{/* <Button type="primary">Save Changes</Button> */}
-</div>
-    </div>
-</div>
-  );
+                    <Col span={8}>
+
+                    </Col>
+                </Row>
+                <div className='saveButton'>
+                    {/* <Button type="primary">Save Changes</Button> */}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default () => <UserProfile />;
