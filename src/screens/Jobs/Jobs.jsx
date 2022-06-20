0b4078f98147115b91fb899855198a512990eb5f
rankothnc <ncrankoth@gmail.com>
2022-06-20 15:06:44 +0530

    created buttons in jobs home

--- src/screens/Jobs/Jobs.jsx
+++ src/screens/Jobs/Jobs.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import JobHome from '../../components/JobHome';
-import { Col, Divider, Row, Card, Button, Checkbox, Form, Input, Steps } from 'antd';
-// import "./components/Job.css";
+import { Col, Row, Card, Button } from 'antd';
+import Sidebar from '../../components/Sidebar';
+import "../../components/Job.css";
 
 function Jobs() {
     return (
         <div>
-            {/* <JobHome /> */}
+            {/* <Sidebar/> */}
             <div className='Background'>
                 <div className='Cards'>
                     <Row gutter={16}>
                         <Col span={8}>
                             <Card bordered={false}>
-                                <h1>Internal Jobs</h1>               
+                                <h1>Internal Jobs</h1><hr />
+                                
+                                <Button className='Button' type="primary">New job requests</Button><br /><br />
+                                <Button className='Button' type="primary">Jobs in Progress</Button><br /><br />
+                                <Button className='Button' type="primary">Completed Jobs</Button><br /><br />
                             </Card>
                         </Col>
 
                         <Col span={8}>
                             <Card bordered={false}>
-                                <h1>External jobs</h1>
+                                <h1>External jobs</h1><hr />
+                                <Button className='Button' type="primary">New job requests</Button><br /><br />
+                                <Button className='Button' type="primary">Jobs in Progress</Button><br /><br />
+                                <Button className='Button' type="primary">Completed Jobs</Button><br /><br />
                             </Card>
                         </Col>
                     </Row>
