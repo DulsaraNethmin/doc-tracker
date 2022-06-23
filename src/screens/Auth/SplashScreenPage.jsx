ace038e0a725fc65d34742de9fbc6fc23184f223
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-24 01:49:33 +0530

    updated positions - login , create org buttons

--- src/screens/Auth/SplashScreenPage.jsx
+++ src/screens/Auth/SplashScreenPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "antd";
 import "./pages.css";
-import { Button } from "antd";
+import { Button, PageHeader } from "antd";
 import { useNavigate } from "react-router-dom";
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -12,6 +12,37 @@ const SplashScreenPage = () => {
   const navigate = useNavigate();
   return (
     <div className="main-container">
+      <PageHeader
+        avatar={{
+          src: "https://avatars1.githubusercontent.com/u/8186664?s=460&v=4",
+        }}
+        //ghost={false}
+        //onBack={() => window.history.back()}
+        title="DocTracker"
+        //subTitle="Organization Mode"
+        extra={[
+          // <Button key="3">Operation</Button>,
+          <Button
+            type="text"
+            onClick={(e) => {
+              navigate("organization/create");
+            }}
+          >
+            Create Organization
+          </Button>,
+          <Button
+            key="1"
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            onClick={(e) => {
+              navigate("/login");
+            }}
+          >
+            Login
+          </Button>,
+        ]}
+      ></PageHeader>
       <div>
         <h1>DOC-TRACKER</h1>
         <br />
@@ -31,7 +62,7 @@ const SplashScreenPage = () => {
           eveniet porro.
         </p>
       </div>
-      <div>
+      {/* <div>
         <br />
         <Button
           type="primary"
@@ -50,7 +81,7 @@ const SplashScreenPage = () => {
         >
           Create Organization
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
