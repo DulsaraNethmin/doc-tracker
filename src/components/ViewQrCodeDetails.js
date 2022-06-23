af75949616a03ea806edd4267c2d78d6c868b65e
sangeethnavodya <pererakasn.19@uom.lk>
2022-06-23 04:15:31 -0700

    Add npm i qrcode

--- src/components/ViewQrCodeDetails.js
+++ src/components/ViewQrCodeDetails.js
@@ -1,8 +1,38 @@
+
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react';
+import QRCode from 'qrcode';
+import { useState } from 'react';
 
 function ViewQrCodeDetails() {
+    const [src,setSrc]=useState('');
+    const doc_id=localStorage.getItem('doc_id');
+    const id={doc_id}.doc_id;
+ useEffect(() => {
+   UserProfile()
+   
+ }, [])
+ useEffect(()=>{
+    
+ },[])
+ 
+    const UserProfile = async() => {
+
+    var response = await axios.get(`http://localhost:8080/document/verify?doc_id=${id}`);
+    console.log(response.data);
+    return response.data;
+        };
+        
+    
   return (
-    <div>ViewQrCodeDetails</div>
+  
+    <div >
+
+         <p>Document id is {id}</p>  
+         <h3>QR Code is</h3>
+         <img src={src} />
+    </div>
   )
 }
 
