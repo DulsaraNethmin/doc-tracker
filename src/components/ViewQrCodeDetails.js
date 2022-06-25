8c72b4c095e2db1878cf8edc3c176a5ae043c3c9
sangeethnavodya <pererakasn.19@uom.lk>
2022-06-25 15:07:51 +0530

    Change size 0f the qr code

--- src/components/ViewQrCodeDetails.js
+++ src/components/ViewQrCodeDetails.js
@@ -6,6 +6,10 @@ import QRCode from 'qrcode';
 import { useState } from 'react';
 
 function ViewQrCodeDetails() {
+    const [doc,setDoc]=useState([]);
+    const [doc_name,setDoc_name]=useState('');
+    const [current_user_name,setcurrent_user_name]=useState('');
+    const [type,settype]=useState('');
     const [src,setSrc]=useState();
     const doc_id=localStorage.getItem('doc_id');
     const id={doc_id}.doc_id;
@@ -22,8 +26,14 @@ function ViewQrCodeDetails() {
  
     const Doc_det = async() => {
 
-    var response = await axios.post(`http://localhost:8080/document/verify?doc_id=${id}`);
+    var response = await axios.get(`http://localhost:8080/document/verify?doc_id=${id}`);
     console.log(response.data);
+    setDoc(response.data);
+    response.data.map((e)=>{
+         setDoc_name(e.doc_name);
+         setcurrent_user_name(e.current_user_name);
+         settype(e.type);
+      })
     return response.data;
         };
         
@@ -31,11 +41,24 @@ function ViewQrCodeDetails() {
   return (
   
     <div >
-
-         <p>Document id is {id}</p>  
+         <div style={{
+            margin:'10px',
+            position:'relative',
+            left:'100px',
+            border: '1px solid green',
+            
+         }}
+         >
+         <h1>Document name is {doc_name} </h1>  
+         <h3>Type is {type}</h3>
          <h3>QR Code is</h3>
-         <p><img src={src} width="100px" 
-     height="100px" /></p>
+         </div>
+         <p style={{
+            width:'300px',
+            position: 'relative',
+  left: '300px'
+         }}><img src={src} width="300px" 
+     height="300px" /></p>
     </div>
   )
 }
