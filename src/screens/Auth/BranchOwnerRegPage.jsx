416bc953276200273731392f3cf0ac16ead18a30
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-27 01:36:59 +0530

    pageheader

--- src/screens/Auth/BranchOwnerRegPage.jsx
+++ src/screens/Auth/BranchOwnerRegPage.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./pages.css";
-import { Form, Input, Button, Row, Col, Card, Checkbox } from "antd";
+
+import {
+  Layout,
+  Button,
+  Dropdown,
+  Menu,
+  PageHeader, Checkbox,
+  Row,
+  Col,
+  Card,
+  Form,
+  Input,
+  Select,
+  Tag,
+  Typography,
+} from "antd";
+const { Paragraph } = Typography;
+const { Header, Footer, Sider, Content } = Layout;
 import { useNavigate } from "react-router-dom";
 import { UserOutlined, HomeOutlined } from "@ant-design/icons";
 import { useState } from "react";
@@ -74,6 +91,48 @@ const BranchOwnerRegPage = () => {
   const navigate = useNavigate();
   return (
     <div>
+      <div className="site-page-header-ghost-wrapper">
+        <PageHeader
+          avatar={{
+            src: "logo.jpeg",
+          }}
+          //ghost={false}
+          //onBack={() => window.history.back()}
+          title="DocTracker"
+          subTitle="Organization Mode"
+          extra={[
+            <Button
+              key="3"
+              onClick={(e) => {
+                navigate("/organization/dashboard");
+              }}
+            >
+              Organization Dashboard
+            </Button>,
+            <Button
+              key="2"
+              onClick={(e) => {
+                navigate("/branch/create");
+              }}
+            >
+              Create New Branch
+            </Button>,
+            <Button
+              key="1"
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              onClick={async (e) => {
+                e.preventDefault();
+                localStorage.clear();
+                navigate("/");
+              }}
+            >
+              Logout
+            </Button>,
+          ]}
+        ></PageHeader>
+      </div>
       <Row style={{ padding: "4% 0" }}>
         <Col span={3}></Col>
         <Col span={18}>
