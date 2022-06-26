2efec4d3ea03b3107fa5cb63b7fda483e3221b80
Harsha-Amarasinghe <harshamad006@gmail.com>
2022-06-27 03:21:45 +0530

    Branch Classnames changed

--- src/screens/Organization/OrganizationDashboard.jsx
+++ src/screens/Organization/OrganizationDashboard.jsx
@@ -116,13 +116,12 @@ const OrganizationDashboard = () => {
         </header>
       </Layout>
 
-      <OrganizationTable />
-      <BranchesTable />
-
       <div>
         <Row style={{ padding: "4% 0" }}>
           <Col span={3}></Col>
           <Col span={18}>
+          <OrganizationTable />
+      <BranchesTable />
             <Card
               title="Enter Username and Password of a branch to edit"
               alignment="center"
